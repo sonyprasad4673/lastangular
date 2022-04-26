2d0834a6e1e643e3395643946047fb0e41aa6896
durga <durgakorada96@gmail.com>
2022-04-26 12:51:30 +0530

    majhas

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/first-app'));
 app.get('/*', function(_req, res)
 
-    { res.sendFile(path.join(__dirname + '/dist/first-app/index.html')); });
+    { res.sendFile(path.join(__dirname + '/dist/first-app')); });
 
 app.listen(process.env.PORT || 8080);
