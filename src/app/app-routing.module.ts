4a58d27285a27bfb6434df717fb7360aa8260190
durga <durgakorada96@gmail.com>
2022-04-26 11:22:00 +0530

    Added new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: "header", component:HeaderComponent},
   {path:"about us",component:AboutUsComponent},
-  {path:"login",component:LoginComponent}
+  {path:"login",component:LoginComponent},
+  {path:"contact-us",component:ContactUsComponent}
   ];
 
 @NgModule({
